066115521d311e296ca6b20e55c40785caaef841
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-16 15:21:58 +0200

    added footer content

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,9 +9,21 @@ export default {
 </script>
 
 <template>
-<main>
-    <h1>FOOTER</h1>
-</main>
+<div>
+    <section id="footer-menu">
+<ul>UL 1</ul>
+<ul> UL 2</ul>
+<ul> UL 3</ul>
+<ul> UL 4</ul>
+    </section>
+    <section id="sign-in-social">
+<a href="#">SIGN-UP NOW!</a>
+<div>
+    <span>FOLLOW US</span>
+    <i></i><i></i><i></i><i></i><i></i>
+</div>
+    </section>
+</div>
 </template>
 
 <style scoped>
