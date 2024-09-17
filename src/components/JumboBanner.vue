28bd9f22d294c47881ebd713c1fe0527951ce741
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-17 17:24:27 +0200

    added css details

--- src/components/JumboBanner.vue
+++ src/components/JumboBanner.vue
@@ -10,6 +10,12 @@ export default {
 
 <template>
     <main>
+        <div class="jumbo-content">
+            <div class="jumbo-text">
+                <h2>TEEN TITANS:</h2> <span>Watch the new episodes on DC-TV!</span>
+            </div>
+
+        </div>
 
     </main>
 
@@ -17,9 +23,35 @@ export default {
 
 <style lang="scss" scoped>
 main {
+
+
+    // css
+
     height: 500px;
     background-image: url(../assets/img/jumbotron.jpg);
     background-size: cover;
     background-repeat: no-repeat;
+
+    position: relative;
+
+
+    .jumbo-content {
+        width: 100%;
+        height: 100%;
+        background-color: rgba($color: #000000, $alpha: 0.4);
+        padding: 30px;
+
+        position: relative;
+
+        .jumbo-text {
+            font-size: 30px;
+            color: gold;
+
+            position: absolute;
+            right: 30px;
+            bottom: 50px;
+        }
+    }
+
 }
 </style>
